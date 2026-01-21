948af3f8c136f402b6a0378bd6f4964d547bfbc5
Aubrey Anup Martin <18359906+AubreyMartin@users.noreply.github.com>
2026-01-21 23:35:07 +0530

    Update styles and pages

--- src/pages/RecurringBills.tsx
+++ src/pages/RecurringBills.tsx
@@ -1,10 +1,125 @@
+import { useEffect, useState } from 'react';
+import type { Transaction } from '../types';
+
+
 function Recurring() {
+  const [data, setData] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((res) => res.json())
+      .then((transationdata: { transactions: Transaction[] }) => {
+
+        setData(transationdata.transactions);
+      });
+  }, []);
+
+
   return (
-    <div className="page recurring">
-      <h1>Recurring Bills</h1>
-      <p>Recurring bills page content will go here</p>
-    </div>
-  )
+    <div>
+      <h1 className="title">Recurring Bills</h1>
+
+      <div className="page-recurring">
+
+        <div className="total-summary">
+          <div className="page-recurring-totalbills">
+            <div className="page-recurring-totalbills-icon">  icon
+            </div>
+            <div>
+              <h3>Total Bills</h3>
+              <div>$384.98</div>
+            </div>
+          </div>
+          <div className="page-recurring-summary">
+            <h1>Summary</h1>
+            <div className="page-recurring-summary-PB" >
+              <div>Paid Bills </div>
+              <div>4 ($190.00)</div></div>
+            <div className="page-recurring-summary-PB">
+              <div >Total Upcoming </div>
+              <div>4 ($194.98)</div></div>
+            <div className="page-recurring-summary-PB">
+              <div>Due Soon </div>
+              <div>$2 ($59.98)</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="page-recurring-billslogs">
+
+
+
+          <div className="page-recurring-controls">
+
+            <div>
+              <input
+                type="text"
+                placeholder="Search transactions"
+                className="recurring-search"
+              />
+            </div>
+
+            <div className='recurring-controls-dropdown text-preset-4'>Sort by
+              <div><select className="transaction-sort">
+                <option value="latest">Latest</option>
+                <option value="oldest">Oldest</option>
+                <option value="az">A to Z</option>
+                <option value="za">Z to A</option>
+                <option value="highest">Highest</option>
+                <option value="lowest">Lowest</option>
+              </select></div>
+            </div>
+          </div>
+
+          <div className='recurring-descriptions'>
+            <p className='recurring-elements-bill-title text-preset-5'>Bill Title</p>
+            <p className='recurring-elements-due-date text-preset-5' >Due Date</p>
+            <p className='recurring-elements-amount text-preset-5'>Amount</p>
+          </div>
+
+
+
+
+
+
+
+
+
+
+          <div className="recurring-log">
+            {data
+              .filter(transaction => transaction.recurring)
+
+              .map((transaction) => (
+                <div className="recurring-row" key={transaction.id}>
+
+                  <div className="recurring-dp-name">
+                    <img src={transaction.avatar} alt="" className="dp" />
+                    <p className="label text-preset-4-bold">{transaction.name}</p>
+                  </div>
+
+                  <div className="recurring-row-cta text-preset-5 ">
+                    <p className="cta-dates text-preset-5">
+                      Monthly – {new Date(transaction.date).getDate()}
+                      {['th', 'st', 'nd', 'rd'][((new Date(transaction.date).getDate() + 90) % 100 - 10) % 10] || 'th'}
+                    </p>
+
+                    <p className="cta-amount text-preset-4-bold"> ${transaction.amount}</p>
+                  </div>
+                </div>
+              ))}
+
+          </div>
+
+
+
+
+        </div>
+
+      </div>
+    </div >
+  );
 }
 
 export default Recurring
+  ;
--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,13 +1,6 @@
 import { useEffect, useState } from 'react';
 
-type Transaction = {
-  id: string | number;
-  avatar: string;
-  name: string;
-  amount: number;
-  date: string;
-  category: string;
-};
+import type { Transaction } from '../types';
 
 function Transactions() {
   const [data, setData] = useState<Transaction[]>([]);
@@ -28,12 +21,36 @@ function Transactions() {
       <div className="page-transactions">
 
         {/*=======Input field  page ======== */}
-        <div className="transaction-inputfield">
-          <div>Search transaction</div>
+        <div className="transaction-controls">
 
-          <div className='transaction-inputfield-dropdown'>
-            <div>Sort by</div>
-            <div>Category</div>
+
+          <div>
+            <input
+              type="text"
+              placeholder="Search transactions"
+              className="transaction-search"
+            />
+          </div>
+
+          <div className='transaction-controls-dropdown text-preset-4'>Sort by
+            <div><select className="transaction-sort">
+              <option value="latest">Latest</option>
+              <option value="oldest">Oldest</option>
+              <option value="az">A to Z</option>
+              <option value="za">Z to A</option>
+              <option value="highest">Highest</option>
+              <option value="lowest">Lowest</option>
+            </select></div>
+
+            Category
+            <div>
+              <select className="transaction-category" >
+                <option value="all">All Transactions</option>
+                <option value="food">Food</option>
+                <option value="rent">Rent</option>
+                <option value="travel">Travel</option>
+                <option value="shopping">Shopping</option>
+              </select> </div>
           </div>
         </div>
 
@@ -41,10 +58,13 @@ function Transactions() {
         {/*=======Transaction description  ======== */}
 
         <div className='transaction-description'>
-          <p>Recipient/Sender</p>
-          <p>Category</p>
-          <p>Transaction Date</p>
-          <p>Amount</p></div>
+          <p className='transaction-name'>Recipient/Sender</p>
+          <div className='transaction-elements'>
+            <p className='transaction-elements-category text-preset-5'>Category</p>
+            <p className='transaction-elements-transactiondate text-preset-5' >Transaction Date</p>
+            <p className='transaction-elements-amount text-preset-5'>Amount</p>
+          </div>
+        </div>
 
 
         {/*=======Transaction Log  ======== */}
@@ -52,24 +72,22 @@ function Transactions() {
 
         <div className="transaction-log">
           {data.map((transaction) => (
-
-
-            <div className="dp-name" key={transaction.id}>
-              <div>
+            <div className="transaction-row" key={transaction.id}>
+              <div className="dp-name">
                 <img src={transaction.avatar} alt="" className="dp" />
                 <p className="label text-preset-4-bold">{transaction.name}</p>
               </div>
 
               <div className="cta text-preset-5 ">
-                <p className="category text-preset-5"> Category</p>
-                <p className="dates  text-preset-5">
+                <p className="cta-category text-preset-5">  {transaction.category}</p>
+                <p className="cta-dates  text-preset-5">
                   {new Date(transaction.date).toLocaleDateString('en-GB',
                     {
                       day: 'numeric',
                       month: 'short',
                       year: 'numeric',
                     })}</p>
-                <p className="amount text-preset-4-bold"> ${transaction.amount}</p>
+                <p className="cta-amount text-preset-4-bold"> ${transaction.amount}</p>
               </div>
             </div>
           ))}
@@ -78,20 +96,21 @@ function Transactions() {
 
 
 
-
-
-      </div>
-      {/*=======Transaction footer  ======== */}
-      <div className='transaction-footer'>
-        <button> Pev</button>
-        <div >
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+        {/*=======Transaction footer  ======== */}
+        <div className='transaction-footer'>
+          <div className='transaction-footer-buttons'>
+            <button className='transaction-footer-buttons-pev'> Pev</button>
+            <div className='transaction-footer-pages-1-4'>
+              <button className='transaction-footer-pages-button' >1</button>
+              <button className='transaction-footer-pages-button'>2</button>
+              <button className='transaction-footer-pages-button'>3</button>
+              <button className='transaction-footer-pages-button'>4</button>
+              <button className='transaction-footer-pages-button'>5</button>
+            </div>
+            <button className='transaction-footer-buttons-next'>Next</button>
+          </div>
         </div>
-        <button>Next</button>
+
       </div>
     </div >
   );
